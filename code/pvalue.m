0c7cdc0ba324cbeed87631beb95fa12392cfaa9b
jeewon_yoon <117123945+jeewon-yoon@users.noreply.github.com>
2023-01-24 23:06:13 -0600

    delete

--- code/pvalue.m
+++
@@ -1,7 +0,0 @@
-for i = 1:1:116
-    for j = 1:1:116
-        kval=[]; kind=[];
-        [kval,kind] = sort(squeeze(Value(i,j,:)),'ascend');
-        pval(i,j) = find(kind==5001)/5001;
-    end
-end
